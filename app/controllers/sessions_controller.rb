c5a8d7f672e3cc405bc509d55847bf6fcfd3f99b
ken1flan <ken1flan@gmail.com>
2015-08-15 14:28:08 +0900

    Add user sigh out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,16 @@
 class SessionsController < ApplicationController
   def create
-      @user = User.find_from(auth_hash)
-      unless @user
-        @user = User.create_with_identity(auth_hash)
-      session[:user_id] = @user.id
-      redirect_to root_path
+    @user = User.find_from(auth_hash)
+    unless @user
+      @user = User.create_with_identity(auth_hash)
     end
+    session[:user_id] = @user.id
+    redirect_to root_path
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to "/", notice: "signed out"
   end
 
   protected
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root "top#index"
   get "/auth/:provider/callback", to: "sessions#create"
+  get "/signout", to: "sessions#destroy", as: :signout
 end
