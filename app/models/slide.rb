1abd779948652d82c033f77100aa7cde2061e2be
kogomi_tan <kogomi_tan@text-poi.net>
2015-08-30 00:05:23 +0900

    Add Slide#generate_token

--- app/models/slide.rb
+++ app/models/slide.rb
@@ -41,4 +41,8 @@ class Slide < ActiveRecord::Base
   validates :user_id,
     presence: true,
     numericality: { greater_than: 0, only_integer: true, allow_blank: true }
+
+  def generate_token
+    self.token = SecureRandom.urlsafe_base64
+  end
 end
